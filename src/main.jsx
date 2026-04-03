2d873a280d3e623d83adf844f39c1fc842c898f9
gopanmodak <gopanhridoy@gmail.com>
2026-04-03 23:35:52 +0600

    update pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './Routes.jsx'
-import AuthProvider from './AuthProvider/AuthProvider.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes.jsx";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
     <AuthProvider>
-
-    <RouterProvider router={routes}/>
-
+      <RouterProvider router={routes} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: "green",
+              secondary: "white",
+            },
+          },
+        }}
+      />
     </AuthProvider>
   </StrictMode>,
-)
+);
